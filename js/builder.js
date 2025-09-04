b4f3117f69fa39612f5191f6ce9db1aa3fe0167e
E-ctrl-coder <elbagire@gmail.com>
2025-09-04 22:21:59 +0100

    Update builder.js

--- js/builder.js
+++ js/builder.js
@@ -1,1 +1,17 @@
+function initGrammarBuilder() {
+  const container = document.getElementById('builder-ui');
+  container.innerHTML = `
+    <label>الجذر:</label><input type="text" id="root-input"><br>
+    <label>الزمن:</label>
+    <select id="tense-select">
+      <option value="past">ماضٍ</option>
+      <option value="present">مضارع</option>
+    </select>
+    <br>
+    <button id="validate-btn">تحقق</button>
+  `;
 
+  document.getElementById('validate-btn').addEventListener('click', () => {
+    alert('Validation logic will go here.');
+  });
+}
