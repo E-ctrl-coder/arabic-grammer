cd4b1d274869597b304a9b9753ea97ec99994cf7
E-ctrl-coder <elbagire@gmail.com>
2025-09-04 22:20:53 +0100

    Update glossary.js

--- js/glossary.js
+++ js/glossary.js
@@ -1,1 +1,10 @@
+let glossary = {};
 
+async function loadGlossary() {
+  const res = await fetch('data/glossary.json');
+  glossary = await res.json();
+}
+
+function getTooltipData(term) {
+  return glossary[term] || null;
+}
